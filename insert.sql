d82886c9411b249cddc048022aa815e64547d032
tj-bu-17 <tj-bu-17@DESKTOP-GFP7CJO>
2023-01-25 11:47:21 +0900

    인설트

---
+++ insert.sql
@@ -0,0 +1,24 @@
+/*
+-- Query: SELECT * FROM sampledb.menu
+LIMIT 0, 1000
+
+-- Date: 2023-01-25 11:46
+*/
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (0,'옛날통닭',9000,10,'image/옛날통닭.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (1,'닭강정',10000,10,'image/닭강정.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (2,'똥집튀김',10000,10,'image/똥집튀김.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (3,'순살튀김',10000,10,'image/순살튀김.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (4,'윙봉',10000,10,'image/윙봉.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (5,'닭다리',14000,10,'image/닭다리.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (6,'무뼈닭발',14000,10,'image/무뼈닭발.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (7,'오뎅탕',6000,10,'image/오뎅탕.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (8,'떡볶이',5000,10,'image/떡볶이.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (9,'치즈볼',4000,10,'image/치즈볼.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (10,'케이준감자',4000,10,'image/케이준감자.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (11,'씨앗호떡',4000,10,'image/씨앗호떡.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (12,'코카콜라',1500,10,'image/코카콜라.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (13,'사이다',1500,10,'image/사이다.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (14,'참이슬',4000,10,'image/참이슬.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (15,'처음처럼',4000,10,'image/처음처럼.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (16,'생맥주',4000,10,'image/생맥주.png');
+INSERT INTO `` (`num`,`name`,`price`,`stock`,`address`) VALUES (17,'카스',4000,10,'image/카스.png');
